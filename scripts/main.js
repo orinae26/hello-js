550f110692b56cdf775a422e2627a0c5e296da8c
Orina <oryna.rich26@gmail.com>
2022-01-19 11:31:24 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+
+//declaring  and initializing variables
+
+let myVariable = "Hello World";
+console.log(myVariable);
+
+//data types
+
+//strings
+let myString = "Hello World";
+
+//numbers
+let myNumber = 5;
+
+//booleans
+let myBoolean = true;
+
+//arrays
+let myArray = [1, 2, 3, 4, 5];
+
+//objects
+let myObject = {address: "123 Main St", city: "New York", state: "NY"};
+
+//comments
+/* multiline
+comment */
+
+//operators
+//addition
+1+1;
+//subtraction
+1-1;
+//multiplication
+1*1;
+//division
+1/1;
+//equality
+1===1;
+
+//objects
+
+let person = { 
+    name:"John",
+    age : 30,
+    isMarried : false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
