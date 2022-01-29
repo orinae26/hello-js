d7ad423fda79c7a2bfa8fc66fffc71267dc32bdd
Orina <oryna.rich26@gmail.com>
2022-01-29 21:01:22 +0300

    implementation of slices

--- scripts/main.js
+++ scripts/main.js
@@ -72,6 +72,25 @@ for(let city of cities){
 }
 
 
+const stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
+    'GNF576746573fhdg4737dh4;Greenfield',
+    'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+    'SYB4f65hf75f736463;Stalybridge',
+    'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
+
+for(station of stations){
+
+    const code = station.slice(0,3);
+
+    const semiColon = station.indexOf(";");
+
+    const name = station.slice(semiColon + 1);
+
+    const newStationName = `${code} : ${name}`;
+
+    console.log(newStationName);
+
+}
 
 
 
