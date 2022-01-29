253a7a80b9c47d1b7ee44733fd403232d8d83253
Orina <oryna.rich26@gmail.com>
2022-01-29 19:26:56 +0300

    Strings and its methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,161 +1,42 @@
+//String Concatenation
+//template literals ('${}')s';
 
-//declaring  and initializing variables
+const strinng1 = `I like the new format of javasscript.
+You do not have to use the backslash to escape the new line character.`
+console.log(strinng1);
 
-let myVariable = "Hello World";
-console.log(myVariable);
+const string = "This is a string";
 
-//data types
+console.log(string.length);
 
-//strings
-let myString = "Hello World";
+const browser = "Mozilla";
 
-//numbers
-let myNumber = 5;
+console.log(browser);
 
-//booleans
-let myBoolean = true;
+//print last character of string
 
-//arrays
-let myArray = [1, 2, 3, 4, 5];
+console.log(browser[browser.length - 1]);
 
-//objects
-let myObject = {address: "123 Main St", city: "New York", state: "NY"};
-
-//comments
-/* multiline
-comment */
-
-//operators
-//addition
-1+1;
-//subtraction
-1-1;
-//multiplication
-1*1;
-//division
-1/1;
-//equality
-1===1;
-
-//objects
-
-let person = { 
-    name:"John",
-    age : 30,
-    isMarried : false,
-    address :{
-        street : "123 Main St",
-        city : "New York",
-        state : "NY"
-    }
-
-}
-
-//retrieve values
-
-console.log(person.name);
-console.log(person.age);
-console.log(person.isMarried);
-
-
-
-//let objct =(Object.values(person));
-console.log(Object.values(person.address));
-
-//Arrays
-
-let names = ["Sam","Alex", "Maria","John"];
-//retrieve elements from an array, index starts at 0
-console.log(names[0]);
-
-//loop through an array
-
-for (n of names){
-    // print each element
-
-    console.log(n);
-}
-
-//index of an element
-names.forEach(function(name, index){
-    console.log(index + " => " + name);
-});
-
-
-//functions
-//addition
-function addition(x,y){
-    return x+y; ;
-};
-console.log(addition(4,6));
-
-//subtraction
-function subtraction(x,y){
-    return x-y;
-};
-
-console.log(subtraction(6,4));
-
-//multiplication
-function multiplication(x,y){
-    return x*y;
-};
-
-console.log(multiplication(4,6));
-
-//division
-function division(x,y){
-    return x/y;
-};
-
-console.log(division(6,4));
-
-//modulus
-
-function modulos(x,y){
-    return x%y;
-};
-
-console.log(modulos(6,4));
-
-
-//string methods
-
-let vehicle = "John Smith";
-
-//toUpperCase method
-
-console.log(vehicle.toUpperCase());
-
-//toLowerCase method
-
-console.log(vehicle.toLowerCase());
-
-
-//loops
-//for loop
-
-
-//prints i 10 times: 0 to 9
-for (i=0; i<=10;i++){
-    console.log(i);
-}
-
-//while loop
-
-let x = 0;
-
-while (x<10){
-    console.log("x is less than 10");
-    x++;
-}
-
-//conditional statements
-let iceCream = "vanilla";
-if(iceCream ==="chocolate"){
-    console.log("Yay, I love chocolate ice cream!");
+//testing if a string contains a substring
+if(browser.includes("zilla")){
+    console.log("Yes, it includes zilla");
 }else{
-    console.log("Aww, chocolate is better.");
+    console.log("No, it does not include zilla");
+}
+
+//.startsWith()
+
+if (browser.startsWith("Moz")){
+    console.log("Yes, it starts with Moz");
+}else{
+    console.log("No, it does not start with Moz");
+}
+
+//.endsWith()
+if(browser.endsWith("zilla")){
+    console.log("Yes, it ends with zilla");
+}else{
+    console.log("No, it does not end with zilla");
 }
 
 
