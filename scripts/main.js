b5c6bd19351594cb5a6e7452edb4ae55e1e9eecb
Orina <oryna.rich26@gmail.com>
2022-01-31 11:34:28 +0300

    conditionals. For...of loops, while and do..while loops

--- scripts/main.js
+++ scripts/main.js
@@ -225,6 +225,88 @@ for(const product  of products){
     console.log(textItem)
 }
 
+//for ...of loop
+
+const cats = ["Leonard","Penny","Raj","Emmy","Bernadette","Sheldon"]
+
+for (const cat of cats){
+    console.log(cat)
+}
+
+//map() 
+
+function toUpper(string){
+    return string.toUpperCase()
+}
+
+const upperCats = cats.map(toUpper)
+
+console.log(upperCats)
+
+//filter ()
+
+function bigCats(string){
+    return string.length > 6
+}
+
+const bigFive = cats.filter(bigCats)
+ 
+console.log(bigFive)
+
+//function expression
+
+const jungle = ["Leopard","Lion", "Zebra","Cheetah","Jaguar"]
+
+const filtered = jungle.filter((cat) => cat.startsWith("L"))
+
+console.log(filtered)
+
+console.log(jungle.length)
+
+for(let i = 0; i < jungle.length; i++){
+    console.log(jungle[i])
+}
+
+let myFavorite = "My favorite Animals are "
+
+for(let i =0; i < jungle.length; i++){
+    if (i === jungle.length - 1 ){
+        myFavorite = `${myFavorite} and ${jungle[i]}`
+    }else{
+        myFavorite = `${myFavorite} ${jungle[i]}`
+    }
+}
+
+console.log(myFavorite)
+
+const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+
+for (const contact of contacts){
+    const splitContact = contact.split(":")
+    console.log(splitContact)
+}
+
+const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+const para = document.querySelector('p');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    const searchName = input.value.toLowerCase();
+    input.value = '';
+    input.focus();
+    para.textContent = '';
+    for (const contact of contacts) {
+        const splitContact = contact.split(':');
+        if (splitContact[0].toLowerCase() === searchName) {
+            para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
+            break;
+        }
+    }
+    if (para.textContent === '') {
+        para.textContent = 'Contact not found.';
+    }
+});
 
 
 
