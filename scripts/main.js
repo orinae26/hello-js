59b7ff06a608ab4ff1a3c6bcc11e171801714433
Orina <oryna.rich26@gmail.com>
2022-02-22 07:41:10 +0300

    constructors

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,80 @@
 //Objects
 const person = {
-  name:['Bob', 'Smith'],
+  name:{
+    first:'Bob',
+    last: 'Smith'},
   age:32,
-  bio: function (){
-    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  bio(){
+    console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.`);
   },
 
-  introduceSelf: function (){
-    console.log(`Hi, my name is ${this.name[0]}`);
+  introduceSelf(){
+    console.log(`Hi, my name is ${this.name.last}`);
   }
 };
+
+//get properties of objects
+//dot notation
+console.log(person.name.first);
+
+console.log(person.age);
+ console.log(person.bio());
+
+
+ //bracket notation
+ console.log(person["name"]["first"]);
+
+ console.log(person["age"]);
+
+ console.log(person["bio"]());
+
+ console.log(person["introduceSelf"]());
+
+//setting properties of objects
+
+person["name"]["first"] = "Harry";
+
+person["name"]["last"] = "Styles";
+
+console.log(person["name"]["first"]);
+
+person.age = 35;
+
+person.farewell = function(){
+  console.log(`${this.name.first} ${this.name.last} has left the building, bye for now.`);
+}
+person.eyes = "blue";
+
+//Constructors
+
+function Person(name){
+  this.name = name;
+  this.age = 0;
+  this.introduceSelf = function(){
+    console.log(`Hi, my name is ${this.name}`);
+  }
+}
+
+//call Person constructor using new keyword
+
+const frank = new Person ('Frank');
+frank.name;
+frank.age;
+frank.introduceSelf();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
