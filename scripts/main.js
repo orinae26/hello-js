635cefb53b9d43f99ce6e484e0ffe8bf513ef847
Orina <oryna.rich26@gmail.com>
2022-02-27 14:34:40 +0300

    Object Prototypes

--- scripts/main.js
+++ scripts/main.js
@@ -65,7 +65,59 @@ console.log(frank.name);
 console.log(frank.age);
 console.log(frank.introduceSelf());
 
+//object prototype:in-built property of objects
 
+const myCity = {
+  name : 'New York',
+
+  population: 8000000,
+
+  greetings (){
+    console.log( `Greetings from ${this.name}`);
+  }
+}
+
+console.log(myCity.greetings());
+
+//get prototype of object myCity
+
+console.log(Object.getPrototypeOf(myCity));
+
+//shadowing properties
+const myDate = new Date(1921,3,04);
+
+console.log(myDate.getYear());
+
+myDate.getYear = function (){
+  console.log(`Its the end of the world as we know it.`);
+}
+console.log(myDate.getYear());
+
+//setting a prototype
+//using object.create(
+
+const personPrototype = {
+  greet (){
+    console.log(`Hello there Beautiful`)
+  }
+}
+const carl = Object.create(personPrototype);
+console.log(carl.greet())
+//using a constructor with object personPrototype and method greet
+
+const personPrototype = {
+  greet (){
+    console.log(`Hello, my name is ${this.name}`);
+  }
+
+}
+
+function Person2 (name){
+  this.name = name;
+}
+
+Person2.prototype = personPrototype; //set person prototype property to point at personPrototype
+Person2.prototype.constructor = Person2; //set person constructor property to the fuction used to create Person objects
 
 
 
